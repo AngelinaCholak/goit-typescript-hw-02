ee847e94b318871abbb22b844cd3ac43a692ff04
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-12-07 15:34:16 +0400

    TS

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,31 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+// class Component {
+//   constructor (public props:T) {
 
-  }
+//   }
+// }
+
+// class Page extends Component {
+//   pageInfo () {
+//     console.log(this.props.title);
+//   }
+// }
+
+// export {};
+
+interface Props {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends Props> {
+  constructor(public props: T) {}
+}
+
+class PageComponent extends Component<Props> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
-
-export {};
+export { PageComponent };
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,26 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+// interface KeyValuePair {
+//   key;
+//   value;
+// }
+
+// export {};
+
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
-export {};
+const pair1: KeyValuePair<string, number> = { key: 'age', value: 25 };
+
+
+const pair2: KeyValuePair<number, boolean> = { key: 10, value: true };
+
+// function printKeyValue<K, V>(pair: KeyValuePair<K, V>) {
+//   console.log(`Key: ${pair.key}, Value: ${pair.value}`);
+// }
+
+// printKeyValue(pair1); 
+// printKeyValue(pair2);
